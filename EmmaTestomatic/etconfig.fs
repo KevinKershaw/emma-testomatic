b7218f0348482188285dbda21ebffb5ab21bba7e
Kevin <admin@msrb.org>
2014-10-29 17:34:34 -0400

    Fix config load problem. (KK)

--- EmmaTestomatic/etconfig.fs
+++ EmmaTestomatic/etconfig.fs
@@ -3,6 +3,7 @@ open FSharp.Configuration
 
 type ETConfig = YamlConfig<"config.yaml">
 let config = ETConfig()
+config.Load ".\config.yaml"
 
 let mutable baseEmmaUrl = config.baseEmmaUrl.ToString()
 let mutable baseDataportUrl = config.baseDataportUrl.ToString()
