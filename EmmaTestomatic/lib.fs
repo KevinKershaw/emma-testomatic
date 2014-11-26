d13bb365230bba38340c9329b52fda698028410e
Kevin <admin@msrb.org>
2014-11-26 14:42:52 -0500

    Add lib functions. (KK)

--- EmmaTestomatic/lib.fs
+++ EmmaTestomatic/lib.fs
@@ -1,4 +1,6 @@
 ﻿module lib
+open OpenQA.Selenium
+open OpenQA.Selenium.Interactions
 open System
 open canopy
 open canopy.configuration
@@ -13,17 +15,11 @@ let isChrome () =
 let isFirefox () =
     (browser :? OpenQA.Selenium.Firefox.FirefoxDriver)
 
-let getSubmissionId (value1 : string) =
-    try
-        let s = (read value1)
-        let b = s.IndexOf("(")
-        let e = s.IndexOf(")")
-        s.Substring((b+1), (e-b-1))
-    with
-    | _ -> ""
-
 let setFieldValue (f : string) (v : string) =
     f << v
+
+let getFieldValue (f : string) =
+    read f
 
 let assertFieldValue (f : string) (v : string) =
     f == v
@@ -39,3 +35,49 @@ let assertDisplayed (f : string) =
 
 let assertNotDisplayed (f : string) =
     notDisplayed f
+
+let assertEqual expected actual =
+    is expected actual
+
+let getRandomNumberString (size : int) =
+    let rnd = System.Random()
+    let max = pown 10 size
+    (rnd.Next max).ToString("00")
+
+let fileUploadSelectPdf (value1 : string) = 
+    let fileName = (AppDomain.CurrentDomain.BaseDirectory  + @"sample.pdf")
+    (element value1).SendKeys(fileName)
+
+let switchToWindow window =
+    browser.SwitchTo().Window(window) |> ignore
+
+let getOtherWindow currentWindow =
+    browser.WindowHandles |> Seq.find (fun w -> w <> currentWindow)
+
+let switchToOtherWindow currentWindow =
+    switchToWindow (getOtherWindow currentWindow) |> ignore
+
+let closeOtherWindow currentWindow =
+    switchToOtherWindow currentWindow
+    browser.Close()
+
+let mouseOver (f : string) =
+    new Actions(browser)
+        |> fun a -> a.MoveToElement(element f)
+        |> fun a -> a.Build()
+        |> fun a -> a.Perform()
+
+let extendTimeout (fn : unit -> unit) =
+    let origCompareTimeout = configuration.compareTimeout
+    try
+        configuration.compareTimeout <- 120.0 
+        fn ()
+    finally
+        configuration.compareTimeout <- origCompareTimeout
+
+let waitForAjax () =
+    let t0 () =
+        sleep 0.01
+        let result = js "return $.active;" |> Convert.ToInt32
+        result = 0
+    waitFor t0
